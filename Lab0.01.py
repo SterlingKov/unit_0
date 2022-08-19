39436b9ee0d82be9e7cc01b0949840931463c9ba
sterling <srkovash25@students.bmchs.org>
2022-08-19 08:21:18 -0500

    finished

--- Lab0.01.py
+++ Lab0.01.py
@@ -14,3 +14,47 @@ and build a function to combine two colors.
     and returns a color that is the sum of the two reds, greens, 
     and blues. Don't forget: the maximum value for R, G, or B is 255.
 '''
+RED = (255,0,0)
+GREEN=(0,255,0)
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+
+class Color():
+    def __init__(self, color):
+        self.color = color
+        self.r = self.color[0]
+        self.g = self.color[1]
+        self.b = self.color[2]
+        
+
+color1 = Color(RED)
+color2 = Color(GREEN)
+color3 = Color(BLUE)
+
+def add_color(c1,c2):
+    nc = Color(BLACK)
+    if c1.r + c2.r > 255:
+        nc.r = 255
+    else:
+        nc.r = c1.r + c2.r
+    if c1.g + c2.g > 255:
+        nc.g = 255
+    else:
+        nc.g = c1.g + c2.g
+    if c1.b + c2.b > 255:
+        nc.b = 255
+    else:
+        nc.b = c1.b + c2.b
+    nc.color = nc.r,nc.g,nc.b
+    return nc
+
+
+color4 = add_color(color1,color2)
+
+color5 = add_color(color2,color3)
+
+color6 = add_color(color1,color3)
+
+print(color4.r,color4.g,color4.b)
+print(color5.r,color5.g,color5.b)
+print(color6.r, color6.g,color6.b)
